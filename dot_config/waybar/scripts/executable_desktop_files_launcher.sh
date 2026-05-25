c3d9437a22bca5d9eac0b6ebe3394d886b136425
JEC15 <86543250+JEC15@users.noreply.github.com>
2026-05-25 05:29:31 -0300

    Waybar: Change desktop launcher to fuzzel.

---
+++ dot_config/waybar/scripts/executable_desktop_files_launcher.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+fuzzel --render-workers=2 --hide-prompt -a top-left --font="IosevkaTerm Nerd Font Mono" -b 1d2021ff -t d4be98ff        \
+-s d4be98ff --prompt-color=d4be98ff --input-color=d4be98ff -S 1d2021ff -B 0
+
