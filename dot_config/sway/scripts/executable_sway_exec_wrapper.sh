88813a3b348faa00fcfdba06b2157423c9468257
JEC15 <86543250+JEC15@users.noreply.github.com>
2026-06-17 19:42:25 -0300

    Add exec wrapper script and config.

---
+++ dot_config/sway/scripts/executable_sway_exec_wrapper.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Start an application in its own Cgroup to avoid a single memory-hungry service
+# taking the entire Sway session down with it, since systemd-oomd tracks resource
+# usage by and kills entire Cgroups at once, not processes.
+# https://wiki.archlinux.org/title/Sway#Manage_Sway-specific_daemons_with_systemd
+
+# Applications run this way should appear as .scope
+# when checked with "systemd-cgls -u user.slice"
+
+exec systemd-run --user --ignore-failure --scope --unit "app-$1-$(uuidgen)" -- "$@"
